6db072b5e00140accaf6f44aea5ae20da421aa45
Encryptium <71296035+Encryptium@users.noreply.github.com>
2022-06-17 17:55:15 -0400

    v1.0.0

---
+++ unlight/index.js
@@ -0,0 +1,23 @@
+const dmode = {
+	version: "v1.0.0",
+	darkModeActive: false, // Access 'darkMode' to check if darkMode is on
+	activateDarkMode: function(backgroundColor="#141414", colors = {}) {
+		document.body.style.backgroundColor = backgroundColor;
+		allElements = document.querySelectorAll("body *");
+		for (let i = 0; i < allElements.length; i++) {
+			allElements[i].style.color = "white";
+		}
+
+		for (var element in colors) {
+      if (colors.hasOwnProperty(element)) {
+        // console.log(element + " -> " + colors[element]);
+				var selectedElements = document.querySelectorAll(element);
+				for (let i = 0; i < selectedElements.length; i++) {
+					selectedElements[i].style.color = colors[element];
+				}
+      }
+    }
+
+		this.darkMode = true;
+	}
+}
